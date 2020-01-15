efc1e85856d5d62fcb422aad5cb38ecb370478c3
aaron <10828507@my.uvu.edu>
2020-01-15 11:07:28 -0700

    syntax + data types

---
+++ New Unity Project/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string nameString;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    
+    void Start()
+    {
+        print(nameString);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Project #1/Assets/Scenes/Scripts/NewClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewClass : MonoBehaviour
+{
+    public string stringName;
+    public int intNum;
+    public float floatNum;
+    public GameObject gameObj;
+    void Start()
+    {
+        print(stringName);
+        print(intNum);
+        print(floatNum);
+        print(gameObj);
+    }
+
+    void Update()
+    {
+        
+    }
+}
