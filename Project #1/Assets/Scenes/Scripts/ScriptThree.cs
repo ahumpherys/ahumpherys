9b6dfe23a66d3ca8a0bb8dd182778f0820081698
Aaron <10828507@my.uvu.edu>
2020-01-22 10:56:25 -0700

    comment comment comment
    122345

---
+++ Project #1/Assets/Scenes/Scripts/ScriptThree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptThree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("I don't know how to code in C# yet except for the print command so that's all I'll be doing right now.");
+        print("Hello World");
+        print("Testing Testing 1 2 3");
+        print("This is a line of code");
+        print("Code Code Code");
+        print("blu blum blah");
+        print("Classes can't have numbers or symbles in the class name");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
