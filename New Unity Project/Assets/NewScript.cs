2cf18dbdc168ef52a4ce97fb8424e09c1cfdc641
ahumpherys <aaronhumpherys@icloud.com>
2020-01-26 16:46:14 -0700

    Today's 25 lines of code maybe.
    everything is broken so IDK.

---
+++ New Unity Project/Assets/NewScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewScript : MonoBehaviour
+{
+    int myInt = 5;
+    void Start()
+    {
+        // partially coding along C# variables and functions video on canvas
+        myInt = multiplyByTwo(myInt);
+      Debug.Log(myInt);
+      print("The myInt variable is changed from 5 to");
+      print(myInt);
+      print("code is complex");
+      //adding some comments
+      //because I don't have enough lines of code
+    }
+    int multiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}
