28f6101dfd593639d31fd1c3e62d3ceb27c31fb7
Aaron <10828507@my.uvu.edu>
2020-01-27 11:04:57 -0700

    Auto stash before merge of "master" and "origin/master"

---
+++ Project #1/Assets/Scenes/Scripts/TriggerEvents.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEvents : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+    //Trigger so when object hits other it names the object
+    //private void Ontriggerenter(collider.other)
+    //print(other.name);
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }
+}
